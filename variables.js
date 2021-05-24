676fac68ef070ac4b10b842ba2b1becf01e31ed2
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-05-24 17:26:48 -0400

    Segunda clase variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+var nombre = "Pedro";
+
+// declarar una variable
+var edad;
+// inicialiazar una variable
+edad = 27;
+
+
+var elementos = ["computadora", "celular"];
+
+var persona = {
+    nombre = "Pedro",
+    edad = 27
+}
+
+persona;
