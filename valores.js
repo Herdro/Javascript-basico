83d29024885f1f385e4f593554a8460718b96e4a
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-05-24 17:16:29 -0400

    Primera clase variables, funciones y sintaxis

---
+++ valores.js
@@ -0,0 +1,16 @@
+// valores Primitivos
+    // valores numericos
+    40
+    // valores en texto
+    "Pedro Herrera"
+    // valores boolean
+    true
+    false
+    // valores vacios
+    null
+    undefined
+// valores objetos
+    // valor array
+    [1,2,3]
+    // valor objeto
+    { nombre: "pedro"}
