9f9d047b0ced0f14cbc45dde8ee55d38ad2d44a5
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-05-24 17:47:55 -0400

    Tercera clase Funciones

---
+++ funciones.js
@@ -0,0 +1,15 @@
+// funciones declarativas
+
+function miFuncion() {
+    return 3;
+}
+
+miFuncion();
+
+// funciones de expresion
+
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion();
