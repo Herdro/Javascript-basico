eca0bccab71c24607986e2d806ec0f7fcc463dfc
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-05-25 15:43:32 -0400

    Quinta clase Hoisting

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+// Declarar una variable
+var miNombre;
+//Inicializar una variable
+miNombre = "Pedro";
+// Declarar e inicializar una variable
+var miNombre = "pedro";
+// ejemplo hoisting
+console.log(miNombre);
+
+var miNombre = "pedro";
